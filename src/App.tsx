89c19aa91406bab2ce61def66dd45021be43690c
Marcelo Andrés Quevedo <marcelo.quevedo@fildelivery.com>
2024-06-05 20:39:28 -0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <hr />
+    </>
+  );
+}
+
+export default App;
