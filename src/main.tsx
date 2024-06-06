3d1fbe81d12890ac5b072bfadbba4c1539d07237
Marcelo Andrés Quevedo <marcelo.quevedo@fildelivery.com>
2024-06-05 20:48:35 -0400

    Initial setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import HeroesApp from "./HeroesApp";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <HeroesApp />
   </React.StrictMode>
 );
