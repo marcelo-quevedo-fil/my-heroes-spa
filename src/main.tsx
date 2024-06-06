4a6515d21bc8308b16be9563bfaa555fe8a3ea00
Marcelo Andrés Quevedo <marcelo.quevedo@fildelivery.com>
2024-06-05 21:05:44 -0400

    Define routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -7,6 +9,8 @@ import HeroesApp from "./HeroesApp";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HeroesApp />
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
   </React.StrictMode>
 );
